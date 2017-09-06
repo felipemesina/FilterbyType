e7dd6924e6fdfdf352dc31f2e9df45f60a6dec65
Felipe <felipejmesina@gmail.com>
2017-09-05 17:41:30 -0700

    assignment completed

---
+++ filterBytype.py
@@ -0,0 +1,17 @@
+int = 0
+if int >= 100:
+    print "That's a big number!"
+elif int< 100:
+    print "That's a small number"
+
+str = "hello my name if felipe mesina and i am attending coding dojo!"
+if len(str)> 50:
+    print "Long sentence"
+elif len(str)< 50:
+    print "Short sentence"
+
+list = [1,2,3,4,5,6,7,8,9]
+if list> 10: 
+    print "Big List!"
+elif list<10:
+    print "Short List!"
